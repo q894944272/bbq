ce8157db1d036421debcc0201f7a6b044b848db8
bbq <894944272@qq.com>
2019-03-03 00:35:43 +0800

    4

--- bbqtb/src/bbq/tb/controller/BaController.java
+++ bbqtb/src/bbq/tb/controller/BaController.java
@@ -13,7 +13,7 @@ import bbq.tb.entity.Bmanager;
 import bbq.tb.entity.Relation;
 import bbq.tb.service.BaService;
 
-//8713453443s
+//8713453443sd
 @Controller
 public class BaController {
 	@Autowired
